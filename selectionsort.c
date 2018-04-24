7a164ebf96b9abb7086353809c3324c29fc3c9c1
yqwung <1522943999@qq.com>
2018-04-24 07:24:21 -0700

    选择排序

---
+++ selectionsort.c
@@ -0,0 +1,32 @@
+#include "selectionsort.h"
+
+
+static void swap(int *a, int *b)
+{
+	int val = 0;
+	
+	val = *a;
+	*a = *b;
+	*b = val;
+}
+
+
+// 选择排序
+void selection_sort(int *arr, int n)
+{
+    int i = 0, j = 0;
+	int len = n;
+	int min = 0;
+	
+	for(i=0; i<len; i++)
+	{
+		min = i;
+		for(j=i+1; j<len; j++)
+		{
+			if(arr[j] < arr[min])			
+				min = j;		
+		}
+		swap(&arr[i], &arr[min]);
+	}
+}
+
---
+++ selectionsort.h
@@ -0,0 +1,8 @@
+#ifndef __SELECTION_SORT_H
+#define __SELECTION_SORT_H
+
+
+void selection_sort(int *arr, int n);
+
+
+#endif
